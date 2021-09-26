52db1455ca62dbea142b8d9ad4003b955aa71dd6
John Ashley <johnashleyworkemail@gmail.com>
2021-09-26 12:56:16 -0400

    added exercise25 pseudocode/skeleton

---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,36 @@
+package baseline;
+import java.util.Scanner;
+
+
+//take in password using method takeUserInput
+//use an integer that changes for each of the following:
+    //check if the password contains letters and not numbers/specials and is <8 char
+    //check if the password contains letters and not numbers and <8
+    //check if the password contains letters/numbers & >8
+    //check if the password contains num, letter, and special and >8
+//output the password's strength based on the returned integer
+
+
+public class Solution25 {
+
+
+
+    public static void main(String[] args)
+    {
+
+    }
+
+
+    private String takeUserInput()
+    {
+
+    }
+
+
+
+    private int passwordStrengthIndicator()
+    {
+
+    }
+
+}
