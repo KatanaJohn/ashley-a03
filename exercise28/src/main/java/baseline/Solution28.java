bd22e7ed87bcdf8229c8729cc6763198ab34fe1d
John Ashley <johnashleyworkemail@gmail.com>
2021-09-27 19:10:48 -0400

    added exercise28 pseudocode/skeleton

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,29 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 John Ashley
+ */
+
+
+package baseline;
+import java.util.Scanner;
+
+//make function prompting for a number
+//create a for loop within that function that is done 5 times
+//have a sum variable that adds the value input by the user to the sum
+
+
+public class Solution28 {
+    private static final Scanner input = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        //create new solution
+        Solution28 solution = new Solution28();
+    }
+
+    private int calculator()
+    {
+        
+    }
+
+}
