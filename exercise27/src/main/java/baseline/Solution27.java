07164ab78653ca8f3ffe269862dc19523242a8ea
John Ashley <johnashleyworkemail@gmail.com>
2021-09-27 17:39:05 -0400

    added exercise27 pseudocode/skeleton

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,76 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 John Ashley
+ */
+
+
+package baseline;
+import java.util.Scanner;
+
+
+/*prompt for first name
+prompt for last name
+prompt for zip code
+prompt for employee id
+have a validation function for each input
+check if every input was validated.
+ */
+
+
+public class Solution27 {
+
+
+    private static final Scanner input = new Scanner(System.in);
+
+
+    public static void main(String[] args)
+    {
+        Solution27 solution = new Solution27();
+    }
+
+    private String promptFirstName()
+    {
+
+    }
+
+    private String promptLastName()
+    {
+
+    }
+
+    private int promptZipCode()
+    {
+
+    }
+
+    private String promptEmployeeID()
+    {
+
+    }
+
+    private String validateFirstName()
+    {
+
+    }
+
+    private String validateLastName()
+    {
+
+    }
+
+    private int validateZipCode()
+    {
+
+    }
+
+    private String validateEmployeeID()
+    {
+
+    }
+
+    private void validateInput(String firstName, String lastName, int zip, String ID)
+    {
+        
+    }
+
+}
