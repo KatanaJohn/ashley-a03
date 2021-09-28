0330ea4fd157c06e363b813b31e73ef4fd480403
John Ashley <johnashleyworkemail@gmail.com>
2021-09-28 13:59:20 -0400

    added exercise31 pseudocode/skeleton

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,46 @@
+package baseline;
+
+
+//function to get heart  rate. Ensure a number is entered
+//function to get age. Ensure a number is entered
+//create a heartRate array and get heartRate
+//display the results
+//TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+
+
+import java.util.Scanner;
+
+public class Solution31 {
+
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+
+        Solution31 solution = new Solution31();
+
+
+    }
+
+
+    private int getHeartRate()
+    {
+
+    }
+
+    private int getAge()
+    {
+
+    }
+
+    private double[] calculateHeartRateArray()
+    {
+
+    }
+
+    private void displayResults()
+    {
+
+    }
+
+}
