a0afffbec3f0b57bba22470297adec60268b5340
John Ashley <johnashleyworkemail@gmail.com>
2021-09-28 15:01:21 -0400

    added exercise33 pseudocode/skeleton

---
+++ exercise33/src/main/java/Solution33.java
@@ -0,0 +1,47 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 John Ashley
+ */
+
+/*
+create array of responses
+create a function to prompt for question
+create function to choose a random response
+ */
+
+
+package baseline;
+import java.util.Scanner;
+
+
+
+public class Solution33 {
+
+    private static final Scanner input = new Scanner(System.in);
+
+
+    public static void main(String[] args)
+    {
+        Solution33 solution = new Solution33();
+    }
+
+    private void promptQuestion()
+    {
+        System.out.println("What's your question?");
+        input.nextLine();
+    }
+
+    private String[] createArray()
+    {
+
+    }
+
+    private void generateResponse()
+    {
+        
+    }
+
+}
+
+
+
