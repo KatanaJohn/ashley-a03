545bb38422ffab50654d97e3d17b79315ea9a6f6
John Ashley <johnashleyworkemail@gmail.com>
2021-09-27 20:12:39 -0400

    added exercise29 pseudocode/skeleton

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,37 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 John Ashley
+ */
+
+
+package baseline;
+import java.util.Scanner;
+
+
+//get rate of return using while loop with a t/f boolean
+//check for if the number is 0 afterwards and stop/loop back.
+//calculate the years to double investment (years = 72/rate)
+//print how long it will take to double investment
+
+public class Solution29 {
+
+    private static final Scanner input = new Scanner(System.in);
+
+
+    public static void main(String[] args)
+    {
+        Solution29 solution = new Solution29();
+
+    }
+
+    private int getRateOfReturn()
+    {
+
+    }
+
+    private int calculateInvestment(int rate)
+    {
+
+    }
+
+}
