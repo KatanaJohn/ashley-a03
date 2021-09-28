8aa6ebdb601e63cd06d5f20c322409408647c20a
John Ashley <johnashleyworkemail@gmail.com>
2021-09-27 21:38:31 -0400

    added exercise30 pseudocode/skeleton

---
+++ exercise30/src/main/java/Solution30.java
@@ -0,0 +1,26 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 John Ashley
+ */
+
+
+package baseline;
+import java.util.Scanner;
+
+//create makeMultiplicationTable function
+//nested for loop using two variables.
+//first loop cycles from first variable starting at 1 up to 12
+//second loop cycles from second variable starting at 1 up to 12.
+//function for doing math
+
+
+public class Solution30 {
+
+    private static final Scanner input = new Scanner(System.in);
+
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
