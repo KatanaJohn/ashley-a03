b5571c9f9ce48f771d45b3c9b7623a10aea1424e
John Ashley <johnashleyworkemail@gmail.com>
2021-09-29 17:19:39 -0400

    added exercise36 code, fix unit testing

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,62 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 John Ashley
+ */
+
+/*
+pseudocode
+create an array list of integers that prompts for numbers until done is entered
+calculate for the average of the numbers
+find the minimum number by cycling through array to find lowest
+find the max number by cycling through array to find highest
+find the population standard deviation
+ */
+
+package baseline;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+public class Solution36 {
+
+    private static final Scanner input = new Scanner(System.in);
+
+
+    public static void main(String[] args)
+    {
+        Solution36 solution = new Solution36();
+        ArrayList<Integer> numberList;
+    }
+
+
+    private ArrayList createNumberList()
+    {
+
+    }
+
+    private void average(ArrayList<Integer> integerList)
+    {
+
+    }
+
+    private void max(ArrayList<Integer> integerList)
+    {
+
+    }
+
+
+    private void min(ArrayList<Integer> integerList)
+    {
+
+    }
+
+    private void std(ArrayList<Integer> integerList)
+    {
+
+    }
+
+
+
+}
