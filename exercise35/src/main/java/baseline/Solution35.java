4ebdcd4ca90be958a178d532b9b86e3de942a60e
John Ashley <johnashleyworkemail@gmail.com>
2021-09-29 14:10:07 -0400

    added exercise35 pseudocode/skeleton

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,65 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 John Ashley
+ */
+
+/*
+- create array/add names to array (no input)
+- display number of employees and names
+- promopt for a name to be removed
+- take that name and remove it from array list
+- display number of employees/names again
+ */
+
+/*
+create arraylist and ask for user input until they put a blank in
+generate a random number
+take that number and get the name that matches the index of that number and return it
+print the winner/the name
+ */
+
+
+package baseline;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+
+
+public class Solution35 {
+
+    private static final Scanner input = new Scanner(System.in);
+
+
+    public static void main(String[] args)
+    {
+        Solution35 solution = new Solution35();
+    }
+
+    private ArrayList createPlayerList()
+    {
+        ArrayList playerList = new ArrayList();
+    }
+
+
+    private int generateNumber(int nPlayers)
+    {
+
+    }
+
+    private String getName(int number, ArrayList playerList)
+    {
+
+    }
+
+
+    private void displayWinner(String winnerName)
+    {
+        
+    }
+
+
+}
+
