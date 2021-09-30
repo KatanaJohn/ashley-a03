658c09b3689c0e179b18679d0c8043d5ca7e4ac6
John Ashley <johnashleyworkemail@gmail.com>
2021-09-30 10:55:21 -0400

    added exercise37 pseudocode/skeleton

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,69 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 John Ashley
+ */
+
+
+/*
+prompt for minimum length of password, special characters, and numbers
+if the number % by 2 is not equal to 0, add 1 to it (for letter = number/SC requirement
+create a list and randomly generate characters into it.
+check for letter/number limit
+put the characters into a string through a for loop
+shuffle the string
+
+ */
+
+package baseline;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+public class Solution37 {
+
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+
+    }
+
+    private String promptPasswordLength()
+    {
+
+    }
+
+    private int promptNumOfSpecial()
+    {
+
+    }
+
+    private int promptNumbers()
+    {
+
+    }
+
+
+    private List<String> generateCharacters()
+    {
+
+    }
+
+    private String createPassword()
+    {
+
+    }
+
+    private String shufflePassword()
+    {
+
+    }
+
+
+
+
+}
+
+
+
