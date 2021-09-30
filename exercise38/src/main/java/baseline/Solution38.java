ce6dbc42e125847cbd0947e360ff028d240c7ac5
John Ashley <johnashleyworkemail@gmail.com>
2021-09-30 12:30:45 -0400

    added exercise38 pseudocode/skeleton

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,48 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 John Ashley
+ */
+
+/*
+//prompt for a list of numbers
+//create a list of new numbers by converting the input to array
+//filter/remove the numbers that are not even numbers from the list
+//print out the new list with the removed numbers
+ */
+
+package baseline;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+public class Solution38 {
+
+    private static final Scanner input = new Scanner(System.in);
+
+
+    public static void main(String[] args)
+    {
+        Solution38 solution = new Solution38();
+    }
+
+    private List promptNewList()
+    {
+
+    }
+
+    private List filterEvenNumbers(List<Integer> numberList)
+    {
+
+    }
+
+    private void printNewList(List<Integer> numberList)
+    {
+
+    }
+
+
+
+}
+
